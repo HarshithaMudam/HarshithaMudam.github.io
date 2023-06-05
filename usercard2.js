3e64a8508b6eb56391669c59f91f2c6ecabfbc53
HarshithaMudam <93461779+HarshithaMudam@users.noreply.github.com>
2023-06-05 23:16:59 +0530

    usercards

--- usercard2.js
+++ usercard2.js
@@ -2,12 +2,12 @@ let alluarjunyounginfo ={
     imgurl: "https://tse1.mm.bing.net/th?id=OIP.cpguvYPuYTC_K3q5q-GHhQHaFg&pid=Api&P=0&h=180",
     name: "Allu Arjun",
     Description: "Indian Actor"
-}
+};
 let alluarjunchildinfo ={
-    imgurl= "https://tse4.mm.bing.net/th?id=OIP.NocGkGQIiQ9Cztc_n58t-gHaJD&pid=Api&P=0&h=180",
+    imgurl: "https://tse4.mm.bing.net/th?id=OIP.NocGkGQIiQ9Cztc_n58t-gHaJD&pid=Api&P=0&h=180",
     name : "Bunny",
     Description: "stylish star"
-}
+};
 let isalluarjunyoung = true;
 let displayobject;
 let flipdata = function(){
@@ -22,4 +22,4 @@ let flipdata = function(){
     document.getElementById("alluarjun-img").src = displayobject.imgurl;
     document.getElementById("alluarjun-name").innerHTML= displayobject.name;
     document.getElementById("alluarjun-designation").innerHTML= displayobject.Description;
-}
+};
