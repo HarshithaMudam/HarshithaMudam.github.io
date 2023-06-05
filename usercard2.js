4fc65fce677976581f9414885da4b8f160d337cb
HarshithaMudam <93461779+HarshithaMudam@users.noreply.github.com>
2023-06-05 22:28:06 +0530

    added js

--- usercard2.js
+++ usercard2.js
@@ -1,5 +1,25 @@
-alluarjuncurinfo ={
+let alluarjuncurinfo ={
     imgurl: "https://tse1.mm.bing.net/th?id=OIP.cpguvYPuYTC_K3q5q-GHhQHaFg&pid=Api&P=0&h=180",
     name: "Allu Arjun",
     Description: "Indian Actor"
+}
+let alluarjunchildhoodinfo ={
+    imgurl= "https://tse4.mm.bing.net/th?id=OIP.NocGkGQIiQ9Cztc_n58t-gHaJD&pid=Api&P=0&h=180",
+    name : "Bunny",
+    Description: "stylish star"
+}
+let isalluarjuncur = true;
+let displayobject;
+let flipdata = function(){
+    if(isalluarjuncur  == true){
+        displayobject = alluarjunchildhoodinfo;
+        isalluarjuncur = false;
+    }
+    else{
+        displayobject = alluarjuncurinfo;
+        isalluarjuncur = true;
+    }
+    document.getElementById("alluarjun-img").src = displayobject.imgurl;
+    document.getElementById("alluarjun-name").src = displayobject.name;
+    document.getElementById("alluarjun-designation").src= displayobject.designation;
 }
