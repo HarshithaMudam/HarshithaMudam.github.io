f3d84ab75b2350b7a3133485566880da6a8e5b49
HarshithaMudam <93461779+HarshithaMudam@users.noreply.github.com>
2023-06-13 22:29:50 +0530

    added image

--- usercard3.js
+++ usercard3.js
@@ -14,10 +14,10 @@ let getRandomUser = function(){
     fetch("https://randomuser.me/api")
     .then(response => response.json())
     .then(data=>{
-        //displayobject.name = data.results[0].name.first + " "+data.results[0].name.last
-        displayobject.name = "User Nmae"
-        //displayobject.imageurl = data.results[0].picture.large
-        //displayobject.Description = data.results[0].genger
+        displayobject.name = data.results[0].name.first + " "+data.results[0].name.last
+        //displayobject.name = "User Name"
+        displayobject.imageurl = data.results[0].picture.large
+        displayobject.Description = data.results[0].genger
         document.getElementById("alluarjun-img").src = displayobject.imgurl;
         document.getElementById("alluarjun-name").innerHTML= displayobject.name;
         document.getElementById("alluarjun-designation").innerHTML= displayobject.Description;
